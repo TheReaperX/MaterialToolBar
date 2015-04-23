5c490c0cedbe68d18599f1eba60494e743365aca
Nana Arkorful <nana.arkorful.12@cnu.edu>
2015-04-23 17:03:06 -0400

    added toolbar element

--- app/src/main/java/com/navdesigns/outlaw/materialtoolbar/MainActivity.java
+++ app/src/main/java/com/navdesigns/outlaw/materialtoolbar/MainActivity.java
@@ -1,17 +1,25 @@
 package com.navdesigns.outlaw.materialtoolbar;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar = null;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar= (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 
 
